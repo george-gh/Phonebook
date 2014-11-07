16c70ced57535e2cbcb6a642c38bea290da13a0b
george-gh <giorgio.bruschini@gmail.com>
2014-11-07 15:39:41 +0100

    Pre-release

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,9 @@
 class Contact < ActiveRecord::Base
 
-  validates :first_name, :last_name, :phone_number, presence: true, allow_blank: false
-  validates :phone_number, uniqueness: true, allow_blank: false
-  validates :phone_number, format: { with: /\A[+]\d{1,3}\s\d{1,3}\s\d{6,}\z/, message: "Invalid phone number" }
+  validates :first_name, :last_name, :phone_number, presence: { message: "Empty field!" }, allow_blank: false
+  # validates :phone_number, uniqueness: true, allow_blank: false
+  validates :phone_number, uniqueness: { message: "Number already in use" }
+  validates :phone_number, format: { with: /\A[+]\d{1,3}\s\d{1,3}\s\d{6,}\z/, message: "Invalid phone number!" }
 
   # Sanitize AR find using hash conditions
   def self.search conditions
